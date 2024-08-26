5d4b844d87641971d04d2c430340070c87ff36e1
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-08-26 16:49:57 +0200

    create aggregator module

---
+++ src/aggregator.rs
@@ -0,0 +1,93 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+use std::time::Duration;
+
+use solana_client::rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient};
+use solana_sdk::clock::Epoch;
+use solana_sdk::commitment_config::CommitmentConfig;
+use solana_sdk::signature::Signature;
+use solana_transaction_status::{EncodedTransaction, UiTransactionEncoding};
+use tokio::time::sleep;
+
+pub struct SolanaAggregator {
+    client: RpcClient,
+    current_epoch: Epoch,
+    transactions: HashMap<Signature, EncodedTransaction>,
+}
+
+impl SolanaAggregator {
+    pub fn new(rpc_url: &str) -> Self {
+        // Create a new RPC client connected to the a Solana RPC URL
+        let client = RpcClient::new(rpc_url);
+
+        let current_epoch = client.get_epoch_info().unwrap().epoch;
+
+        SolanaAggregator {
+            client,
+            current_epoch,
+            transactions: HashMap::new(),
+        }
+    }
+
+    pub async fn fetch_transactions(&mut self) {
+        // Set up the parameters for fetching signatures
+        let mut last_signature: Option<Signature> = None;
+
+        loop {
+            // Fetch the latest signatures for finalized transactions
+            let signature_results = self.client.get_signatures_for_address_with_config(
+                &self
+                    .client
+                    .get_account_with_commitment(
+                        &self.client.get_identity().unwrap(),
+                        CommitmentConfig::finalized(),
+                    )
+                    .unwrap()
+                    .value
+                    .unwrap()
+                    .owner,
+                GetConfirmedSignaturesForAddress2Config {
+                    before: None,
+                    until: last_signature,
+                    limit: Some(1000),
+                    commitment: Some(CommitmentConfig::finalized()),
+                },
+            );
+
+            match signature_results {
+                Ok(signatures) => {
+                    // Process each transaction signature
+                    for signature_info in signatures.iter() {
+                        let signature = Signature::from_str(&signature_info.signature)
+                            .expect("Invalid signature format");
+                        println!("Signature: {}", signature);
+                        match &self
+                            .client
+                            .get_transaction(&signature, UiTransactionEncoding::JsonParsed)
+                        {
+                            Ok(transaction) => {
+                                tracing::info!(
+                                    "📄 Storing transaction with signature {} ({})",
+                                    signature,
+                                    transaction.block_time.unwrap()
+                                );
+                                let transaction = transaction.transaction.transaction.clone();
+                                let _ = &self.transactions.insert(signature, transaction);
+                                last_signature = Some(signature);
+                            }
+                            Err(err) => {
+                                tracing::error!("❌ Error fetching transaction: {}", err);
+                            }
+                        }
+                    }
+                }
+                Err(err) => {
+                    tracing::error!("❌ Error fetching signatures: {}", err);
+                }
+            }
+
+            // Wait for a bit before fetching new transactions
+            sleep(Duration::from_secs(1)).await;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
+mod aggregator;
+
+use aggregator::SolanaAggregator;
 use std::net::SocketAddr;
-use std::str::FromStr;
-use std::time::Duration;
 
 use anyhow::Context;
 use clap::Parser;
-use solana_client::rpc_client::{GetConfirmedSignaturesForAddress2Config, RpcClient};
-use solana_sdk::commitment_config::CommitmentConfig;
-use solana_sdk::signature::Signature;
-use solana_transaction_status::UiTransactionEncoding;
-use tokio::time::sleep;
 use url::Url;
 
 #[derive(Parser, Debug)]
@@ -34,77 +30,14 @@ async fn main() -> anyhow::Result<()> {
         .parse::<SocketAddr>()
         .context("Invalid local address")?;
 
-    println!("RPC URL: {}", rpc_url);
-    println!("Local address: {}", local_address);
+    tracing::info!("🌐 Solana RPC URL: {}", rpc_url);
+    tracing::info!("🛠️ Local RESTful API address: {}", local_address);
 
-    collect_data(&rpc_url).await?;
+    // Create a new Solana aggregator
+    let mut aggregator = SolanaAggregator::new(rpc_url.as_str());
+    aggregator.fetch_transactions().await;
 
     Ok(())
-}
-
-async fn collect_data(rpc_url: &Url) -> Result<(), Error> {
-    // Create a new RPC client connected to the a Solana RPC URL
-    let client = RpcClient::new(rpc_url.to_string());
-
-    // Create a vector to store transactions
-    let mut transactions = Vec::new();
-
-    // Set up the parameters for fetching signatures
-    let mut last_signature: Option<Signature> = None;
-
-    loop {
-        // Fetch the latest signatures for confirmed transactions
-        let signature_results = client.get_signatures_for_address_with_config(
-            &client
-                .get_account_with_commitment(
-                    &client.get_identity().unwrap(),
-                    CommitmentConfig::finalized(),
-                )
-                .unwrap()
-                .value
-                .unwrap()
-                .owner,
-            GetConfirmedSignaturesForAddress2Config {
-                before: None,
-                until: last_signature,
-                limit: Some(1000),
-                commitment: Some(CommitmentConfig::finalized()),
-            },
-        );
-
-        match signature_results {
-            Ok(signatures) => {
-                // Process each transaction signature
-                for signature_info in signatures.iter() {
-                    let signature = Signature::from_str(&signature_info.signature)
-                        .expect("Invalid signature format");
-                    println!("Signature: {}", signature);
-                    match client.get_transaction(&signature, UiTransactionEncoding::JsonParsed) {
-                        Ok(transaction) => {
-                            tracing::info!(
-                                "📄 Storing transaction with signature {} ({})",
-                                signature,
-                                transaction.block_time.unwrap()
-                            );
-                            transactions.push(transaction);
-                            last_signature = Some(signature);
-                        }
-                        Err(err) => {
-                            tracing::error!("❌ Error fetching transaction: {}", err);
-                        }
-                    }
-                }
-            }
-            Err(err) => {
-                tracing::error!("❌ Error fetching signatures: {}", err);
-            }
-        }
-
-        // Wait for a bit before fetching new transactions
-        sleep(Duration::from_secs(1)).await;
-    }
-
-    // Ok(())
 }
 
 pub fn init_tracing() {
