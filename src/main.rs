2fcc3b35c1281b93ba38d47546084455e4218a06
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-08-26 09:43:32 +0200

    add clap arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use anyhow::Context;
+use clap::Parser;
+use url::Url;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The address of the RPC URL for Solana
+    #[arg(short, long, default_value = "https://api.devnet.solana.com/")]
+    rpc_address: String,
+
+    /// The address of this local RESTful API server
+    #[arg(short, long, default_value = "0.0.0.0:0")]
+    local_address: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let rpc_address = args.rpc_address.parse::<Url>().context("Invalid RPC URL")?;
+    let local_address = args
+        .local_address
+        .parse::<SocketAddr>()
+        .context("Invalid local address")?;
+
+    println!("RPC URL: {}", rpc_address);
+    println!("Local address: {}", local_address);
+
+    Ok(())
 }
