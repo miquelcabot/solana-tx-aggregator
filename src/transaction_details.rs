f7c23899fa4c4792a873d7632559b75c703464cf
Miquel A. Cabot <miquel.cabot@gmail.com>
2024-08-27 13:56:13 +0200

    add comments to Transaction details

--- src/transaction_details.rs
+++ src/transaction_details.rs
@@ -1,5 +1,14 @@
 use serde::{Deserialize, Serialize};
 
+/// Transaction details
+/// 
+/// This struct represents the details of a transaction.
+/// 
+/// # Fields
+/// * `sender` - The sender of the transaction
+/// * `receiver` - The receiver of the transaction
+/// * `data` - The data of the transaction
+/// * `timestamp` - The timestamp of the transaction
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TransactionDetails {
     pub sender: String,
